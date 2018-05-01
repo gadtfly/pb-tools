0baf4669bbbb64ffc20ad100ab2c64a2ead57df9
adamlouis <adamlouis@gmail.com>
2018-04-30 21:09:13 -0700

    Add dimensions info, minus other junk
    Also switch CORS-circumventor

--- sku-viewer/sku-viewer.js
+++ sku-viewer/sku-viewer.js
@@ -1,13 +1,29 @@
 const input = document.getElementsByTagName('input')[0];
 const img = document.getElementsByTagName('img')[0];
+const div = document.getElementsByTagName('div')[0];
+
+function takeWhile(xs, f) {
+  const result = [];
+  for (x of xs) {
+    if (!f(x)) break;
+    result.push(x);
+  }
+  return result;
+}
 
 input.addEventListener('input', event => {
-  fetch(`https://cors-anywhere.herokuapp.com/${`https://www.potterybarn.com/products/${input.value}`}`)
+  fetch(`https://cors.now.sh/${`https://www.potterybarn.com/products/${input.value}`}`)
     .then(response => response.text() )
     .then(text => {
-      img.src = (new DOMParser).parseFromString(text, 'text/html').querySelector('[property="og:image"]').content;
+      const doc = (new DOMParser()).parseFromString(text, 'text/html');
+      img.src = doc.querySelector('[property="og:image"]').content;
+      div.innerHTML = '';
+      takeWhile(doc.querySelector('#tab1 .accordion-tab-copy').children, node => !node.textContent.startsWith('CARE AND MAINTENANCE'))
+        .forEach(node => div.appendChild(node));
     })
     .catch(error => {
+      console.log(error);
       img.src = '';
+      div.innerHTML = '';
     });
 });
